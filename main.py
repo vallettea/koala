ae0752b53ea56b7daf360c2eda943b7d8ef3a432
vallettea <vallettea@gmail.com>
2016-04-12 15:19:48 +0200

    added tokenizer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import glob
+from datetime import datetime
+
+
 import os.path
 import warnings
 from io import BytesIO
@@ -6,10 +9,9 @@ from io import BytesIO
 from koala.xml.functions import fromstring, safe_iterator
 from koala.unzip import read_archive
 from koala.excel import read_named_ranges, read_cells
-
+from koala.ast.tokenizer import ExcelParser
 
 if __name__ == '__main__':
-
 
     files = glob.iglob("./data/*.xlsx")
     for file in files:  
@@ -17,14 +19,20 @@ if __name__ == '__main__':
         file_name = os.path.abspath(file)
         print file_name
         
+        startTime = datetime.now()
         archive = read_archive(file_name)        
 
         named_range = read_named_ranges(archive)
-        print "%s named ranged parsed" % str(len(named_range))
+        print "%s named ranged parsed in %s" % (str(len(named_range)), str(datetime.now() - startTime))
 
+        startTime = datetime.now()
         cells = read_cells(archive)
         
-        print "%s cells parsed" % str(len(cells))
-        # for cell in cells:
-        #   if cell['f'] is not None:
-        #         print cell['f']
+        print "%s cells parsed in %s" % (str(len(cells)), str(datetime.now() - startTime))
+        for cell in cells:
+            if cell['f'] is not None:
+                print "=========="
+                print cell['f']
+                p = ExcelParser();
+                p.parse(cell['f'])
+                print p.prettyprint()
