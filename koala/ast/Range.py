0a3eed206a0e3a7381deae45c61964bb79556bfc
Romain Crestey <rcrestey@gmail.com>
2016-04-19 18:06:34 +0200

    added functions to Range class

--- koala/ast/Range.py
+++ koala/ast/Range.py
@@ -6,7 +6,7 @@ CELL_REF_RE = re.compile(r"\!?(\$?[A-Za-z]{1,3})(\$?[1-9][0-9]{0,6})$")
 
 def get_values(ref, first, second = None):
     first_value = None
-    second_value = None
+    second_value = second
 
     try:
         col = re.search(CELL_REF_RE, ref).group(1)
@@ -22,7 +22,7 @@ def get_values(ref, first, second = None):
     if not first_value:
         raise Exception('First argument of Range operation is not valid')
 
-    if second is not None:
+    if type(second) == Range:
         for key, value in second.items():
             r, c = key
             if r == row or c == col:
@@ -48,89 +48,52 @@ class Range(OrderedDict):
     # CAUTION, for now, only 1 dimension ranges are supported
 
     def add(self, other, ref):
-        if type(other) == Range:
-            first, second = get_values(ref, self, other)
-        else:
-            first = get_values(ref, self)[0]
-            second = other
+        first, second = get_values(ref, self, other)
 
         return first + second
 
-    # def substract(self, other, ref):
-    #     first, second = get_values(ref, self, other)
+    def substract(self, other, ref):
+        first, second = get_values(ref, self, other)
 
-    #     return first - second
+        return first - second
 
-        # if type(other) == Range:
-        #     return check_array(self, index) + check_array(other, index)
-        # else:
-        #     return check_array(self, index) + other
+    def multiply(self, other, ref):
+        first, second = get_values(ref, self, other)
 
-    # def substract(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) - check_array(other, index)
-    #     else:
-    #         return check_array(self, index) - other
+        return first * second
 
-    # def multiply(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) * check_array(other, index)
-    #     else:
-    #         return check_array(self, index) * other
+    def divide(self, other, ref):
+        first, second = get_values(ref, self, other)
 
-    # def divide(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) / check_array(other, index)
-    #     else:
-    #         return check_array(self, index) / other
+        return first / second
 
-    # # not sure if this is needed:
+    def is_equal(self, other, ref):
+        first, second = get_values(ref, self, other)
 
-    # # def OR(self, other, index):
-    # #     if type(other) == Range:
-    # #         return check_array(self, index) or check_array(other, index)
-    # #     else:
-    # #         return check_array(self, index) or other
+        return first == second
 
-    # # def AND(self, other, index):
-    # #     if type(other) == Range:
-    # #         return check_array(self, index) and check_array(other, index)
-    # #     else:
-    # #         return check_array(self, index) and other
+    def is_not_equal(self, other, ref):
+        first, second = get_values(ref, self, other)
 
-    # def is_equal(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) == check_array(other, index)
-    #     else:
-    #         return check_array(self, index) == other
+        return first != second
 
-    # def is_not_equal(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) != check_array(other, index)
-    #     else:
-    #         return check_array(self, index) != other
+    def is_strictly_superior(self, other, ref):
+        first, second = get_values(ref, self, other)
 
-    # def is_strictly_superior(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) > check_array(other, index)
-    #     else:
-    #         return check_array(self, index) > other
+        return first > second
 
-    # def is_strictly_inferior(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) < check_array(other, index)
-    #     else:
-    #         return check_array(self, index) < other
+    def is_strictly_inferior(self, other, ref):
+        first, second = get_values(ref, self, other)
 
-    # def is_superior_or_equal(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) >= check_array(other, index)
-    #     else:
-    #         return check_array(self, index) >= other
+        return first < second
 
-    # def is_inferior_or_equal(self, other, index):
-    #     if type(other) == Range:
-    #         return check_array(self, index) <= check_array(other, index)
-    #     else:
-    #         return check_array(self, index) <= other
+    def is_superior_or_equal(self, other, ref):
+        first, second = get_values(ref, self, other)
+
+        return first >= second
+
+    def is_inferior_or_equal(self, other, ref):
+        first, second = get_values(ref, self, other)
+
+        return first <= second
     
--- tests/ast/Range.py
+++ tests/ast/Range.py
@@ -39,96 +39,68 @@ class Test_Excel(unittest.TestCase):
 
         self.assertEqual(Range.add(range, constant, 'C1'), 3) # 1 + 2 = 3
 
-    # def test_add_constant(self):
-    #     self.assertEqual(Range.add(Range([1, 2, 3, 4]), 5, 2), 8) # 3 + 5 = 8
+    # SUBSTRACT
+    def test_substract(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 10, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 1])
 
+        self.assertEqual(Range.substract(range1, range2, 'C2'), 7) # 10 - 3 = 7
     
-    # # SUBSTRACT
-    # def test_substract_array(self):
-    #     self.assertEqual(Range.substract(Range([1, 2, 3, 4]), Range([4, 3, 2, 1]), 2), 1) # 3 - 2 = 1
+    # MULTIPLY
+    def test_multiply(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 10, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 1])
 
-    # def test_substract_constant(self):
-    #     self.assertEqual(Range.substract(Range([1, 2, 3, 4]), 3, 2), 0) # 3 - 3 = 0
+        self.assertEqual(Range.multiply(range1, range2, 'C2'), 30) # 10 * 3 = 30
 
-    # # MULTIPLY
-    # def test_multiply_array(self):
-    #     self.assertEqual(Range.multiply(Range([1, 2, 3, 4]), Range([4, 3, 2, 1]), 2), 6) # 3 * 2 = 6
+    # DIVIDE
+    def test_divide(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 30, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 1])
 
-    # def test_multiply_constant(self):
-    #     self.assertEqual(Range.multiply(Range([1, 2, 3, 4]), 3, 2), 9) # 3 * 3 = 9 
+        self.assertEqual(Range.divide(range1, range2, 'C2'), 10) # 30 / 3 = 10
 
+    # IS_EQUAL
+    def test_is_equal(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 30, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 1])
 
-    # # DIVIDE
-    # def test_divide_array(self):
-    #     self.assertEqual(Range.divide(Range([1.0, 2.0, 3.0, 4.0]), Range([4.0, 3.0, 2.0, 1.0]), 2), 1.5) # 3 / 2 = 1.5
+        self.assertEqual(Range.is_equal(range1, range2, 'C2'), False) # 30 == 3 is False
 
-    # def test_divide_constant(self):
-    #     self.assertEqual(Range.divide(Range([1, 2, 3, 4]), 3, 2), 1) # 3 / 3 = 1
+    # IS_EQUAL
+    def test_is_not_equal(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 30, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 1])
 
-    # # Not sure if this is needed:
+        self.assertEqual(Range.is_not_equal(range1, range2, 'C2'), True) # 30 != 3 is True
 
-    # # # OR
-    # # def test_OR_array(self):
-    # #     self.assertEqual(Range.OR(Range([True, False]), Range([False, False]), 0), False) # True or False is True
+    # IS_STRICTLY_SUPERIOR
+    def test_is_strictly_superior(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 30, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 1])
 
-    # # def test_OR_constant(self):
-    # #     self.assertEqual(Range.OR(Range([True, False]), False, 0), True)  # True or False if True
+        self.assertEqual(Range.is_strictly_superior(range1, range2, 'C2'), True) # 30 > 3 is True
 
+    # IS_STRICTLY_INFERIOR
+    def test_is_strictly_inferior(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 30, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 1])
 
-    # # # AND
-    # # def test_AND_array(self):
-    # #     self.assertEqual(Range.AND(Range([True, False]), Range([False, False]), 0), False) # True and False is False
+        self.assertEqual(Range.is_strictly_inferior(range1, range2, 'C2'), False) # 30 < 3 is False
 
-    # # def test_AND_constant(self):
-    # #     self.assertEqual(Range.AND(Range([True, False]), False, 0), False)  # True and False if False
+    # IS_SUPERIOR_OR_EQUAL
+    def test_is_superior_or_equal(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 30, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 3])
 
+        self.assertEqual(Range.is_superior_or_equal(range1, range2, 'C3'), True) # 3 >= 3 is True
 
-    # # IS_EQUAL
-    # def test_is_equal_array(self):
-    #     self.assertEqual(Range.is_equal(Range([1, 2, 3, 4]), Range([4, 3, 2, 1]), 2), False) # 3 == 2 is False
+    # IS_INFERIOR_OR_EQUAL
+    def test_is_inferior_or_equal(self):
+        range1 = Range(['A1', 'A2', 'A3'], [1, 30, 3])
+        range2 = Range(['B1', 'B2', 'B3'], [3, 3, 3])
 
-    # def test_is_equal_constant(self):
-    #     self.assertEqual(Range.is_equal(Range([1, 2, 3, 4]), 3, 2), True) # 3 == 3 is True 
-
-
-    # # IS_NOT_EQUAL
-    # def test_is_not_equal_array(self):
-    #     self.assertEqual(Range.is_not_equal(Range([1, 2, 3, 4]), Range([4, 3, 2, 1]), 2), True) # 3 != 2 is True
-
-    # def test_is_not_equal_constant(self):
-    #     self.assertEqual(Range.is_not_equal(Range([1, 2, 3, 4]), 2, 2), True) # 3 != 2 is True
-
-
-    # # IS_STRICTLY_SUPERIOR
-    # def test_is_strictly_superior_array(self):
-    #     self.assertEqual(Range.is_strictly_superior(Range([1, 2, 3, 4]), Range([4, 3, 2, 1]), 2), True) # 3 > 2 is True
-
-    # def test_is_strictly_superior_constant(self):
-    #     self.assertEqual(Range.is_strictly_superior(Range([1, 2, 3, 4]), 2, 2), True) # 3 > 2 is True
-
-
-    # # IS_STRICTLY_INFERIOR
-    # def test_is_strictly_inferior_array(self):
-    #     self.assertEqual(Range.is_strictly_inferior(Range([1, 2, 3, 4]), Range([4, 3, 2, 1]), 2), False) # 3 < 2 is False
-
-    # def test_is_strictly_inferior_constant(self):
-    #     self.assertEqual(Range.is_strictly_inferior(Range([1, 2, 3, 4]), 2, 2), False) # 3 < 2 is False
-
-
-    # # IS_SUPERIOR_OR_EQUAL
-    # def test_is_superior_or_equal_array(self):
-    #     self.assertEqual(Range.is_superior_or_equal(Range([1, 2, 3, 4]), Range([3, 3, 3, 3]), 2), True) # 3 >= 3 is True
-
-    # def test_is_superior_or_equal_constant(self):
-    #     self.assertEqual(Range.is_superior_or_equal(Range([1, 2, 3, 4]), 2, 2), True) # 3 >= 2 is True
-
-
-    # # IS_INFERIOR_OR_EQUAL
-    # def test_is_inferior_or_equal_array(self):
-    #     self.assertEqual(Range.is_inferior_or_equal(Range([1, 2, 3, 4]), Range([3, 3, 3, 3]), 2), True) # 3 <= 3 is True
-
-    # def test_is_inferior_or_equal_constant(self):
-    #     self.assertEqual(Range.is_inferior_or_equal(Range([1, 2, 3, 4]), 2, 2), False) # 3 <= 2 is False
+        self.assertEqual(Range.is_inferior_or_equal(range1, range2, 'C1'), True) # 1 <= 3 is False
 
 
 if __name__ == '__main__':
