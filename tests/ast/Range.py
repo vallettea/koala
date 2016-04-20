41062dfaaeb45fd27365a19a841b0a3279515f88
Romain Crestey <rcrestey@gmail.com>
2016-04-20 15:38:35 +0200

    added Range.is_associated_horizontal() tests

--- tests/ast/Range.py
+++ tests/ast/Range.py
@@ -36,6 +36,12 @@ class Test_Excel(unittest.TestCase):
 
         self.assertEqual(range1.is_associated(range2), True)
 
+    def test_is_associated_horizontal(self):
+        range1 = Range(['A1', 'B1', 'C1'], [1, 2, 3])
+        range2 = Range(['A2', 'B2', 'C2'], [1, 2, 3])
+
+        self.assertEqual(range1.is_associated(range2), True)
+
     def test_is_not_associated(self):
         range1 = Range(['A1', 'A2', 'A3'], [1, 2, 3])
         range2 = Range(['B2', 'B3', 'B4'], [1, 2, 3])
