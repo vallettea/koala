7b4e6e89a9ffb217cdcdb5b56ce97f211d624094
vallettea <vallettea@gmail.com>
2016-05-17 14:23:35 +0200

    repaired pruing tests

--- tests/ast/pruning.py
+++ tests/ast/pruning.py
@@ -7,7 +7,18 @@ dir = os.path.dirname(__file__)
 path = os.path.join(dir, '../..')
 sys.path.insert(0, path)
 
-
+# Here is the graph contained in pruning.xlsx
+#       
+#                          test
+#                         /  |  \
+#      A1 B1 C1   D1    G1  H1  I1
+#       \  \ /    /       \  |  /
+#       A2 B2   D2          H2
+#         \/    /          /  
+#         A3   /          /
+#           \ /          /
+#           C6__________/
+#      
 from koala.ast.graph import ExcelCompiler
 
 
@@ -28,6 +39,10 @@ class Test_without_range(unittest.TestCase):
     def test_pruning_cellmap(self):
         self.assertEqual(len(self.sp.cellmap.keys()), 9)
 
+    def test_eval(self):
+        self.sp.set_value('Sheet1!A1', 10)
+        self.assertEqual(self.sp.evaluate('Sheet1!C6'), 38)
+
 class Test_with_range(unittest.TestCase):
     
     def setUp(self):
@@ -37,26 +52,29 @@ class Test_with_range(unittest.TestCase):
         self.sp = c.gen_graph(outputs = ["Sheet1!C6"], inputs = ["Sheet1!A1","Sheet1!B1", "test"])
         
     def test_pruning_nodes(self):
-      self.assertEqual(self.sp.G.number_of_nodes(), 10)
+      self.assertEqual(self.sp.G.number_of_nodes(), 13)
 
     def test_pruning_edges(self):
-        self.assertEqual(self.sp.G.number_of_edges(), 9)
+        self.assertEqual(self.sp.G.number_of_edges(), 12)
 
     def test_pruning_cellmap(self):
-        self.assertEqual(len(self.sp.cellmap.keys()), 10)
+        self.assertEqual(len(self.sp.cellmap.keys()), 13)
 
     def test_A1(self):
         self.sp.set_value('Sheet1!A1', 10)
-        self.assertEqual(self.sp.evaluate('Sheet1!C6'), 34)
+        self.assertEqual(self.sp.evaluate('Sheet1!C6'), 38)
 
     def test_H1(self):
         self.sp.set_value('Sheet1!H1', 4)
-        self.assertEqual(self.sp.evaluate('Sheet1!C6'), 27)
+        self.assertEqual(self.sp.evaluate('Sheet1!C6'), 31)
 
-    # def test_G1(self):
-    #     self.sp.set_value('test', 4) # todo set_value for range
-    #     print "zzzzz", self.sp.evaluate('Sheet1!C6')
-    #     self.assertEqual(self.sp.evaluate('Sheet1!C6'), 27)
+    def test_G1(self):
+        self.sp.set_value('test', 4)
+        self.assertEqual(self.sp.evaluate('Sheet1!C6'), 35)
+
+    def test_G1_bis(self):
+        self.sp.set_value('test', [7,8,9])
+        self.assertEqual(self.sp.evaluate('Sheet1!C6'), 47)
 
 if __name__ == '__main__':
     unittest.main()
