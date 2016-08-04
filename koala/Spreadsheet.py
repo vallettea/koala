262d736e3b1ee07d872686540028dc74c95aef30
Romain Crestey <rcrestey@gmail.com>
2016-08-04 19:15:19 +0200

    fixed precedent commit on freeing cells

--- koala/Spreadsheet.py
+++ koala/Spreadsheet.py
@@ -536,15 +536,15 @@ class Spreadsheet(object):
     def free_cell(self, address = None):
         if address is None:
             for addr in self.fixed_cells:
-                # self.cellmap[addr].should_eval = 'always' # this is to be able to correctly reinitiliaze the value
+                self.cellmap[addr].should_eval = 'always' # this is to be able to correctly reinitiliaze the value
                 self.evaluate(addr)
                 self.cellmap[addr].should_eval = self.fixed_cells[addr]
             self.fixed_cells = {}
         elif address in self.cellmap:
-            # self.cellmap[address].should_eval = 'always' # this is to be able to correctly reinitiliaze the value
+            self.cellmap[address].should_eval = 'always' # this is to be able to correctly reinitiliaze the value
             self.evaluate(address)
             self.cellmap[address].should_eval = self.fixed_cells[address]
-            # self.fixed_cells.pop(address, None)
+            self.fixed_cells.pop(address, None)
         else:
             raise Exception('Cell %s not in cellmap' % address)
 
