98069d7b118e0bc06b9eee4e97ca647281b901f1
Romain Crestey <rcrestey@gmail.com>
2016-08-18 16:20:27 +0200

    v0.0.10

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Cython.Build import cythonize
 setup(
     name = "koala2",
 
-    version = "0.0.9",
+    version = "0.0.10",
 
     author = "Ants, open innovation lab",
     author_email = "contact@ants.builders",
