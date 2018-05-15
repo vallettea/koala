34de384c4c2d40c3575e501d37744be4416eecc6
vallettea <vallettea@gmail.com>
2018-05-15 11:10:51 +0200

    some cleaning

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     setup(
         name = "koala2",
 
-        version = "0.0.19",
+        version = "0.0.20",
 
         author = "Ants, open innovation lab",
         author_email = "contact@weareants.fr",
