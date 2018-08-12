72ff860aac6a0e21a57a9f166715378bb91cbfed
Alexei Tishin <altos.z@gmail.com>
2018-08-12 14:53:13 +0300

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     setup(
         name="koala2",
 
-        version="0.0.21",
+        version="0.0.22",
 
         author="Ants, open innovation lab",
         author_email="contact@weareants.fr",
