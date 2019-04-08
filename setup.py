fc32ec2b669651ee441129b79ef2679daacc0c60
vallettea <vallettea@gmail.com>
2019-04-08 08:43:54 +0200

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     setup(
         name="koala2",
 
-        version="0.0.30",
+        version="0.0.31",
 
         author="Ants, open innovation lab",
         author_email="contact@weareants.fr",
