0653c47b9912685bb3e6a1563ea757b4ff719da6
danielsjf <danielsjf@users.noreply.github.com>
2019-04-18 10:10:20 +0200

    Merge Spreadsheet and ExcelCompiler with backwards compatibility

--- koala/ExcelCompiler.py
+++ koala/ExcelCompiler.py
@@ -21,116 +21,12 @@ class ExcelCompiler(object):
     def __init__(self, file, ignore_sheets = [], ignore_hidden = False, debug = False):
         # print("___### Initializing Excel Compiler ###___")
 
-        file_name = os.path.abspath(file)
-        # Decompose subfiles structure in zip file
-        archive = read_archive(file_name)
-        # Parse cells
-        self.cells = read_cells(archive, ignore_sheets, ignore_hidden)
-        # Parse named_range { name (ExampleName) -> address (Sheet!A1:A10)}
-        self.named_ranges = read_named_ranges(archive)
-        self.Range = RangeFactory(self.cells)
-        self.pointers = set()
-        self.debug = debug
+        self.spreadsheet = Spreadsheet(file = file, ignore_sheets = ignore_sheets, ignore_hidden = ignore_hidden, debug = debug)
 
     def clean_pointer(self):
-        sp = Spreadsheet(networkx.DiGraph(),self.cells, self.named_ranges, debug = self.debug)
-
-        cleaned_cells, cleaned_ranged_names = sp.clean_pointer()
-        self.cells = cleaned_cells
-        self.named_ranges = cleaned_ranged_names
-        self.pointers = set()
+        self.spreadsheet.clean_pointer()
 
     def gen_graph(self, outputs = [], inputs = []):
-        # print('___### Generating Graph ###___')
+        self.spreadsheet
 
-        if len(outputs) == 0:
-            preseeds = set(list(flatten(self.cells.keys())) + list(self.named_ranges.keys())) # to have unicity
-        else:
-            preseeds = set(outputs)
-
-        preseeds = list(preseeds) # to be able to modify the list
-
-        seeds = []
-        for o in preseeds:
-            if o in self.named_ranges:
-                reference = self.named_ranges[o]
-
-                if is_range(reference):
-                    if 'OFFSET' in reference or 'INDEX' in reference:
-                        start_end = prepare_pointer(reference, self.named_ranges)
-                        rng = self.Range(start_end)
-                        self.pointers.add(o)
-                    else:
-                        rng = self.Range(reference)
-
-                    for address in rng.addresses: # this is avoid pruning deletion
-                        preseeds.append(address)
-                    virtual_cell = Cell(o, None, value = rng, formula = reference, is_range = True, is_named_range = True )
-                    seeds.append(virtual_cell)
-                else:
-                    # might need to be changed to actual self.cells Cell, not a copy
-                    if 'OFFSET' in reference or 'INDEX' in reference:
-                        self.pointers.add(o)
-
-                    value = self.cells[reference].value if reference in self.cells else None
-                    virtual_cell = Cell(o, None, value = value, formula = reference, is_range = False, is_named_range = True)
-                    seeds.append(virtual_cell)
-            else:
-                if is_range(o):
-                    rng = self.Range(o)
-                    for address in rng.addresses: # this is avoid pruning deletion
-                        preseeds.append(address)
-                    virtual_cell = Cell(o, None, value = rng, formula = o, is_range = True, is_named_range = True )
-                    seeds.append(virtual_cell)
-                else:
-                    seeds.append(self.cells[o])
-
-        seeds = set(seeds)
-        # print("Seeds %s cells" % len(seeds))
-        outputs = set(preseeds) if len(outputs) > 0 else [] # seeds and outputs are the same when you don't specify outputs
-
-        cellmap, G = graph_from_seeds(seeds, self)
-
-        if len(inputs) != 0: # otherwise, we'll set inputs to cellmap inside Spreadsheet
-            inputs = list(set(inputs))
-
-            # add inputs that are outside of calculation chain
-            for i in inputs:
-                if i not in cellmap:
-                    if i in self.named_ranges:
-                        reference = self.named_ranges[i]
-                        if is_range(reference):
-
-                            rng = self.Range(reference)
-                            for address in rng.addresses: # this is avoid pruning deletion
-                                inputs.append(address)
-                            virtual_cell = Cell(i, None, value = rng, formula = reference, is_range = True, is_named_range = True )
-                            cellmap[i] = virtual_cell
-                            G.add_node(virtual_cell) # edges are not needed here since the input here is not in the calculation chain
-
-                        else:
-                            # might need to be changed to actual self.cells Cell, not a copy
-                            virtual_cell = Cell(i, None, value = self.cells[reference].value, formula = reference, is_range = False, is_named_range = True)
-                            cellmap[i] = virtual_cell
-                            G.add_node(virtual_cell) # edges are not needed here since the input here is not in the calculation chain
-                    else:
-                        if is_range(i):
-                            rng = self.Range(i)
-                            for address in rng.addresses: # this is avoid pruning deletion
-                                inputs.append(address)
-                            virtual_cell = Cell(i, None, value = rng, formula = o, is_range = True, is_named_range = True )
-                            cellmap[i] = virtual_cell
-                            G.add_node(virtual_cell) # edges are not needed here since the input here is not in the calculation chain
-                        else:
-                            cellmap[i] = self.cells[i]
-                            G.add_node(self.cells[i]) # edges are not needed here since the input here is not in the calculation chain
-
-            inputs = set(inputs)
-
-
-        # print("Graph construction done, %s nodes, %s edges, %s cellmap entries" % (len(G.nodes()),len(G.edges()),len(cellmap)))
-
-        # undirected = networkx.Graph(G)
-        # print "Number of connected components %s", str(number_connected_components(undirected))
-
-        return Spreadsheet(G, cellmap, self.named_ranges, pointers = self.pointers, outputs = outputs, inputs = inputs, debug = self.debug)
+        return self.spreadsheet
--- koala/Spreadsheet.py
+++ koala/Spreadsheet.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, print_function
 from koala.Range import get_cell_address, parse_cell_address
 
 from koala.ast import *
+from koala.reader import read_archive, read_named_ranges, read_cells
 # This import equivalent functions defined in Excel.
 from koala.excellib import *
 from openpyxl.formula.translate import Translator
@@ -11,6 +12,7 @@ from koala.serializer import *
 from koala.tokenizer import reverse_rpn
 from koala.utils import *
 
+import os.path
 import networkx
 from networkx.readwrite import json_graph
 
@@ -18,8 +20,151 @@ from openpyxl.compat import unicode
 
 
 class Spreadsheet(object):
-    def __init__(self, G, cellmap, named_ranges, pointers = set(), outputs = set(), inputs = set(), debug = False):
-        super(Spreadsheet,self).__init__()
+    def __init__(self, file=None, ignore_sheets=[], ignore_hidden=False, debug=False):
+        # print("___### Initializing Excel Compiler ###___")
+
+        if file is None:
+            self.cells = None
+            self.named_ranges = None
+            self.range = None
+            self.pointers = None
+            self.debug = None
+
+            self.G = None
+            self.cellmap = None
+            self.named_ranges = None
+            self.addr_to_name = None
+            self.addr_to_range = None
+            self.outputs = None
+            self.inputs = None
+            self.save_history = None
+            self.history = None
+            self.count = None
+            self.pointer_to_remove = None
+            self.pointers = None
+            self.pointers_to_reset = None
+            self.reset_buffer = None
+            self.debug = None
+            self.fixed_cells = None
+        else:
+            super(Spreadsheet, self).__init__()
+            file_name = os.path.abspath(file)
+            # Decompose subfiles structure in zip file
+            archive = read_archive(file_name)
+            # Parse cells
+            self.cells = read_cells(archive, ignore_sheets, ignore_hidden)
+            # Parse named_range { name (ExampleName) -> address (Sheet!A1:A10)}
+            self.named_ranges = read_named_ranges(archive)
+            self.range = RangeFactory(self.cells)
+            self.pointers = set()
+            self.debug = debug
+
+            self.gen_graph()
+
+    def clean_pointer(self):
+        sp = Spreadsheet(networkx.DiGraph(),self.cells, self.named_ranges, debug = self.debug)
+
+        cleaned_cells, cleaned_ranged_names = sp.clean_pointer()
+        self.cells = cleaned_cells
+        self.named_ranges = cleaned_ranged_names
+        self.pointers = set()
+
+    def gen_graph(self, outputs = [], inputs = []):
+        # print('___### Generating Graph ###___')
+
+        if len(outputs) == 0:
+            preseeds = set(list(flatten(self.cells.keys())) + list(self.named_ranges.keys())) # to have unicity
+        else:
+            preseeds = set(outputs)
+
+        preseeds = list(preseeds) # to be able to modify the list
+
+        seeds = []
+        for o in preseeds:
+            if o in self.named_ranges:
+                reference = self.named_ranges[o]
+
+                if is_range(reference):
+                    if 'OFFSET' in reference or 'INDEX' in reference:
+                        start_end = prepare_pointer(reference, self.named_ranges)
+                        rng = range(start_end)
+                        self.pointers.add(o)
+                    else:
+                        rng = range(reference)
+
+                    for address in rng.addresses: # this is avoid pruning deletion
+                        preseeds.append(address)
+                    virtual_cell = Cell(o, None, value = rng, formula = reference, is_range = True, is_named_range = True )
+                    seeds.append(virtual_cell)
+                else:
+                    # might need to be changed to actual cells Cell, not a copy
+                    if 'OFFSET' in reference or 'INDEX' in reference:
+                        self.pointers.add(o)
+
+                    value = self.cells[reference].value if reference in self.cells else None
+                    virtual_cell = Cell(o, None, value = value, formula = reference, is_range = False, is_named_range = True)
+                    seeds.append(virtual_cell)
+            else:
+                if is_range(o):
+                    rng = range(o)
+                    for address in rng.addresses: # this is avoid pruning deletion
+                        preseeds.append(address)
+                    virtual_cell = Cell(o, None, value = rng, formula = o, is_range = True, is_named_range = True )
+                    seeds.append(virtual_cell)
+                else:
+                    seeds.append(self.cells[o])
+
+        seeds = set(seeds)
+        # print("Seeds %s cells" % len(seeds))
+        outputs = set(preseeds) if len(outputs) > 0 else [] # seeds and outputs are the same when you don't specify outputs
+
+        cellmap, G = graph_from_seeds(seeds, self)
+
+        if len(inputs) != 0: # otherwise, we'll set inputs to cellmap inside Spreadsheet
+            inputs = list(set(inputs))
+
+            # add inputs that are outside of calculation chain
+            for i in inputs:
+                if i not in cellmap:
+                    if i in self.named_ranges:
+                        reference = self.named_ranges[i]
+                        if is_range(reference):
+
+                            rng = range(reference)
+                            for address in rng.addresses: # this is avoid pruning deletion
+                                inputs.append(address)
+                            virtual_cell = Cell(i, None, value = rng, formula = reference, is_range = True, is_named_range = True )
+                            cellmap[i] = virtual_cell
+                            G.add_node(virtual_cell) # edges are not needed here since the input here is not in the calculation chain
+
+                        else:
+                            # might need to be changed to actual cells Cell, not a copy
+                            virtual_cell = Cell(i, None, value = self.cells[reference].value, formula = reference, is_range = False, is_named_range = True)
+                            cellmap[i] = virtual_cell
+                            G.add_node(virtual_cell) # edges are not needed here since the input here is not in the calculation chain
+                    else:
+                        if is_range(i):
+                            rng = range(i)
+                            for address in rng.addresses: # this is avoid pruning deletion
+                                inputs.append(address)
+                            virtual_cell = Cell(i, None, value = rng, formula = o, is_range = True, is_named_range = True )
+                            cellmap[i] = virtual_cell
+                            G.add_node(virtual_cell) # edges are not needed here since the input here is not in the calculation chain
+                        else:
+                            cellmap[i] = self.cells[i]
+                            G.add_node(self.cells[i]) # edges are not needed here since the input here is not in the calculation chain
+
+            inputs = set(inputs)
+
+
+        # print("Graph construction done, %s nodes, %s edges, %s cellmap entries" % (len(G.nodes()),len(G.edges()),len(cellmap)))
+
+        # undirected = networkx.Graph(G)
+        # print "Number of connected components %s", str(number_connected_components(undirected))
+
+        return self.build_spreadsheet(G, cellmap, self.named_ranges, pointers = self.pointers, outputs = outputs, inputs = inputs, debug = self.debug)
+
+    def build_spreadsheet(self, G, cellmap, named_ranges, pointers = set(), outputs = set(), inputs = set(), debug = False):
         self.G = G
         self.cellmap = cellmap
         self.named_ranges = named_ranges
