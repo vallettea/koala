bf08767109a799968ff90e341c5729e20b2698fc
danielsjf <danielsjf@users.noreply.github.com>
2019-04-18 11:28:05 +0200

    Add deprication warnings

--- koala/ExcelCompiler.py
+++ koala/ExcelCompiler.py
@@ -11,6 +11,7 @@ from koala.ast import graph_from_seeds, shunting_yard, build_ast, prepare_pointe
 from koala.Cell import Cell
 from koala.Range import RangeFactory
 from koala.Spreadsheet import Spreadsheet
+import warnings
 
 
 class ExcelCompiler(object):
@@ -21,12 +22,26 @@ class ExcelCompiler(object):
     def __init__(self, file, ignore_sheets = [], ignore_hidden = False, debug = False):
         # print("___### Initializing Excel Compiler ###___")
 
+        warnings.warn(
+            "The ExcelCompiler class will disappear in a future version. Please use Spreadsheet instead.",
+            PendingDeprecationWarning
+        )
         self.spreadsheet = Spreadsheet(file = file, ignore_sheets = ignore_sheets, ignore_hidden = ignore_hidden, debug = debug)
 
     def clean_pointer(self):
+
+        warnings.warn(
+            "The ExcelCompiler class will disappear in a future version. Please use Spreadsheet.clean_pointer instead.",
+            PendingDeprecationWarning
+        )
         self.spreadsheet.clean_pointer()
 
     def gen_graph(self, outputs = [], inputs = []):
-        self.spreadsheet
 
+        warnings.warn(
+            "The ExcelCompiler class will disappear in a future version. Please use Spreadsheet.gen_graph() instead. "
+            "Please also note that this function is now included in the init of Spreadsheet and therefore it shouldn't "
+            "be called as such anymore.",
+            PendingDeprecationWarning
+        )
         return self.spreadsheet
