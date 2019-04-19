738fecf5c03bc6e95cadabc1b73d864bc682fbc1
danielsjf <danielsjf@users.noreply.github.com>
2019-04-19 11:44:21 +0200

    Style fix

--- tests/excel/test_spreadsheet.py
+++ tests/excel/test_spreadsheet.py
@@ -10,6 +10,6 @@ class Test_Spreadsheet(unittest.TestCase):
 
         spreadsheet.cell_add('Sheet1!A1', value=1)
         spreadsheet.cell_add('Sheet1!A2', value=2)
-        spreadsheet.cell_add('Sheet1!A3', formula='SUM(Sheet1!A1, Sheet1!A2)')
+        spreadsheet.cell_add('Sheet1!A3', formula='=SUM(Sheet1!A1, Sheet1!A2)')
 
         self.assertEqual(spreadsheet.evaluate('Sheet1!A3'), 3)
