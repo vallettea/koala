009d584fc41bd3f8234b00bc985f0b7468e412e9
danielsjf <danielsjf@users.noreply.github.com>
2019-04-19 15:01:08 +0200

    Test create spreadsheet from scratch

--- tests/excel/test_spreadsheet.py
+++ tests/excel/test_spreadsheet.py
@@ -11,5 +11,9 @@ class Test_Spreadsheet(unittest.TestCase):
         spreadsheet.cell_add('Sheet1!A1', value=1)
         spreadsheet.cell_add('Sheet1!A2', value=2)
         spreadsheet.cell_add('Sheet1!A3', formula='=SUM(Sheet1!A1, Sheet1!A2)')
+        spreadsheet.cell_add('Sheet1!A4', formula='=SUM(Sheet1!A1:A2)')
+        spreadsheet.cell_add('Sheet1!A5', formula='=SUM(Sheet1!A1:A1)')
 
-        self.assertEqual(spreadsheet.evaluate('Sheet1!A3'), 3)
+        self.assertEqual(spreadsheet.evaluate('Sheet1!A3'), 3)  # test function
+        self.assertEqual(spreadsheet.evaluate('Sheet1!A4'), 3)  # test range
+        self.assertEqual(spreadsheet.evaluate('Sheet1!A5'), 1)  # test short range
