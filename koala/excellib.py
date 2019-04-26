dfee1b79e5f7a3c3b79b6608377a5ad59a7eb9ec
Daniels Jef <danielsjf@users.noreply.github.com>
2019-04-26 11:26:40 +0200

    Update excellib.py

--- koala/excellib.py
+++ koala/excellib.py
@@ -990,9 +990,9 @@ def xirr(values, dates, guess=0):
         raise ValueError('guess value for excellib.irr() is %s and not 0' % guess)
     else:
         try:
-            return scipy.optimize.newton(lambda r: xnpv(r, values, dates, lim_rate=False), 0.0)
-        except RuntimeError:  # Failed to converge?
-            return scipy.optimize.brentq(lambda r: xnpv(r, values, dates, lim_rate=False), -1.0, 1e10)
+            return scipy.optimize.newton(lambda r: xnpv(r, values, dates, lim_rate_low=False, lim_rate_high=True), 0.0)
+        except (RuntimeError, FloatingPointError):  # Failed to converge?
+            return scipy.optimize.brentq(lambda r: xnpv(r, values, dates, lim_rate_low=False, lim_rate_high=True), -1.0, 1e10)
 
 
 def vlookup(lookup_value, table_array, col_index_num, range_lookup = True): # https://support.office.com/en-us/article/VLOOKUP-function-0bbc8083-26fe-4963-8ab8-93a18ad188a1
@@ -1128,13 +1128,15 @@ def vdb(cost, salvage, life, start_period, end_period, factor = 2, no_switch = F
     return result
 
 
-def xnpv(rate, values, dates, lim_rate = True):  # Excel reference: https://support.office.com/en-us/article/XNPV-function-1b42bbf6-370f-4532-a0eb-d67c16b664b7
+def xnpv(rate, values, dates, lim_rate_low=True, lim_rate_high=False):  # Excel reference: https://support.office.com/en-us/article/XNPV-function-1b42bbf6-370f-4532-a0eb-d67c16b664b7
     """
     Function to calculate the net present value (NPV) using payments and non-periodic dates. It resembles the excel function XPNV().
 
     :param rate: the discount rate.
     :param values: the payments of which at least one has to be negative.
     :param dates: the dates as excel dates (e.g. 43571 for 16/04/2019).
+    :param lim_rate_low: to limit the rate below 0.
+    :param lim_rate_high: to limit the rate above 1000 to avoid overflow errors.
     :return: a float being the NPV.
     """
     if isinstance(values, Range):
@@ -1146,10 +1148,14 @@ def xnpv(rate, values, dates, lim_rate = True):  # Excel reference: https://supp
     if len(values) != len(dates):
         return ExcelError('#NUM!', '`values` range must be the same length as `dates` range in XNPV, %s != %s' % (len(values), len(dates)))
 
-    if lim_rate and rate < 0:
+    if lim_rate_low and rate < 0:
         return ExcelError('#NUM!', '`excel cannot handle a negative `rate`' % (len(values), len(dates)))
 
+    if lim_rate_high and rate > 1000:
+        return ExcelError('#NUM!', '`will result in an overflow error due to high `rate`' % (len(values), len(dates)))
+
     xnpv = 0
+    np.seterr(all='raise')
     for v, d in zip(values, dates):
         xnpv += v / np.power(1.0 + rate, (d - dates[0]) / 365)
 
