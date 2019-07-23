400ad442b054aff235b64dd02827992eef42afcf
Bradley van Ree <38775942+Brad-eki@users.noreply.github.com>
2019-07-23 22:14:17 +1000

    Improving setup.py

--- setup.py
+++ setup.py
@@ -11,24 +11,52 @@ if __name__ == '__main__':
     setup(
         name="koala2",
 
-        version="0.0.36",
+        version="0.0.37",
 
         author="Ants, open innovation lab",
         author_email="contact@weareants.fr",
 
         packages=find_packages(),
 
+        classifiers=[
+            "Programming Language :: Python :: 2.7 :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: MacOS :: MacOS X',
+        ],
+
         include_package_data=True,
 
         url="https://github.com/vallettea/koala",
 
-        license="GNU GPL3",
         description=(
-            "A python module to extract all the content of an Excel document "
-            "and enable calculation without Excel"
+            "A Python module to extract all the content of an Excel document and enable calculation without Excel."
         ),
         long_description_content_type='text/markdown',
         long_description=long_description,
+
+        keywords=['xls',
+            'excel',
+            'spreadsheet',
+            'workbook',
+            'data analysis',
+            'analysis'
+            'reading excel',
+            'read excel',
+            'excel formula',
+            'excel formulas',
+            'excel equations',
+            'excel equation',
+            'formula',
+            'formulas',
+            'equation',
+            'equations',
+            'timeseries',
+            'time series',
+            'research',
+            'scenario analysis',
+            'modelling',
+            'model'],
 
         install_requires=[
             'networkx >= 2.1',
@@ -42,13 +70,3 @@ if __name__ == '__main__':
             'backports.functools_lru_cache==1.5'
         ]
     )
-
-    networkx==2.1
-    openpyxl==2.5.3
-    numpy>=1.14.2
-    Cython==0.28.2
-    lxml==4.1.1
-    six==1.11.0
-    scipy>=1.0.0
-    python-dateutil==2.8.0
-    backports.functools_lru_cache==1.5
