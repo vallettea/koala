b4a2e7e6e471e86a6671a5ea81518e385dfde36c
Brian May <brian@linuxpenguins.xyz>
2020-01-15 07:40:38 +1000

    Fix CI tests
    This was broken on Python 2.7 by 18f4a6d92fe887132b216615a2fae77255e25945

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 """Setup script."""
 
+from io import open
 from setuptools import setup, find_packages
 from os import path
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
