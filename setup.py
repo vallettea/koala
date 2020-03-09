8442969b43a6f183f8712581f548cf4afffbe3ff
Kyle Monson <kyle@8minutenergy.com>
2020-03-09 14:25:38 -0700

    Fix lxml version.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
             'openpyxl >= 3.0.3',
             'numpy >= 1.14.2',
             'Cython >= 0.29.15',
-            'lxml >= 4.5.1',
+            'lxml >= 4.5.0',
             'scipy>=1.0.0',
             'python-dateutil==2.8.0'
         ]
